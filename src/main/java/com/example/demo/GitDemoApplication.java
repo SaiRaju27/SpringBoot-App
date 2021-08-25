68f207556e5d46b558e27ab38c29f9e359c4f031
SaiRaju27 <kraju9589@gmai.com>
2021-08-25 12:02:02 +0530

    Main class modified

--- src/main/java/com/example/demo/GitDemoApplication.java
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy.Configurable;
 
 @SpringBootApplication
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitDemoApplication.class, args);
+		ConfigurableApplicationContext run= SpringApplication.run(GitDemoApplication.class, args);
+		run.close();
 	}
 
 }
